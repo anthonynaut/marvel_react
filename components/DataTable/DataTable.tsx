62f495260ae622ab7425854baa723b6996c7d2a3
Anthony V. Naut <83789702+anthonynaut@users.noreply.github.com>
2021-07-15 09:21:05 -0400

    Add files via upload

---
+++ components/DataTable/DataTable.tsx
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@material-ui/data-grid';
+import { server_calls } from '../../api';
+import { useGetData } from '../../custom-hooks';
+import { Button,Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle } from '@material-ui/core';
+import { DroneForm } from '../../components/DroneForm';
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    // {
+      // field: 'fullName',
+      // headerName: 'Full name',
+      // description: 'This column has a value getter and is not sortable.',
+      // sortable: false,
+      // width: 160,
+      // valueGetter: (params: GridValueGetterParams)
+        // `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+    // },
+  ];
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+  
+  interface gridData{
+    data:{
+      id?:string;
+    }
+  }
+  
+  export const DataTable =  () => {
+    
+    let { droneData, getData } = useGetData();
+    let [open, setOpen] = useState(false);
+  let [gridData, setData] = useState<gridData>({data:{}})
+  
+  let handleOpen = () => {
+    setOpen(true)
+  }
+  
+  let handleClose = () => {
+    setOpen(false)
+  }
+  
+  let deleteData = () => {
+    server_calls.delete(gridData.data.id!)
+    getData()
+  }
+  
+  console.log(gridData.data.id)
+  
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+          <h2>Drones In Inventory</h2>
+          <DataGrid rows={droneData} columns={columns} pageSize={5} checkboxSelection onRowSelected = { setData } />
+
+        <Button onClick={handleOpen}>Update</Button>
+        <Button variant="contained" color="secondary" onClick={deleteData}>Delete</Button>
+
+          {/*Dialog Pop Up begin */}
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Update Drone Drone</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Update Drone</DialogContentText>
+              <DroneForm id={gridData.data.id!}/>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick = {handleClose} color="primary">Cancel</Button>
+            <Button onClick={handleClose} color = "primary">Done</Button> 
+          </DialogActions>
+        </Dialog>
+        </div>
+      );
+    }
+    
+    export const DataTable = () => {
+      return (
+          <div style={{ height: 400, width: '100%' }}>
+            <h2>Marvel Characters In Inventory</h2>
+            <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+          </div>
+        );
+  }
